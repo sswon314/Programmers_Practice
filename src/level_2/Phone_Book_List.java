114a711a768a2080010185040f27a574c97ca17d
LeeSangWon <sswon314@naver.com>
2022-01-22 19:58:05 +0900

    phone_book_list
    1/22 연습(해쉬)

---
+++ src/level_2/Phone_Book_List.java
@@ -0,0 +1,35 @@
+package level_2;
+
+/* 
+ * 코딩 테스트 연습
+ * 전화번호 목록
+ * https://programmers.co.kr/learn/courses/30/lessons/42577
+ * 핵심 내용 : 해쉬
+ *  */
+
+public class Phone_Book_List {
+	public boolean solution(String[] phone_book) {
+        boolean answer = true;
+        HashSet<String>book=new HashSet<>();
+        for(int i=0;i<phone_book.length;i++){
+            book.add(phone_book[i]);
+        }
+        
+        for(int i=0;i<phone_book.length;i++){
+            book.remove(phone_book[i]);
+            for(int j=1;j<phone_book[i].length();j++){
+                if(book.contains(phone_book[i].substring(0,j))){
+                    answer=false;
+                    break;
+                }
+            }
+            
+            if(answer==false)
+                break;
+            else
+                book.add(phone_book[i]);
+        }
+        
+        return answer;
+    }
+}
